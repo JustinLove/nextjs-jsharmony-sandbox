baf51839f2471c6d0bad799877381689ab459d78
Justin Love <jl@apharmony.com>
2024-04-18 13:35:33 -0500

    import match redirect

--- app/lib/cms_router.tsx
+++ app/lib/cms_router.tsx
@@ -1,12 +1,63 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+//matchRedirect - Check if URL matches redirects and return first match
+//Parameters:
+//  redirects: Array(object) Array of CMS Redirects
+//  urlpath: (string) Target URL path
+//Returns: (object) Redirect Object
+//Redirect Object {
+//  http_code: '301', '302', or 'PASSTHRU',
+//  url: 'destination/url',
+//}
+export function matchRedirect(redirects : RedirectEntry[], urlpath : string) : RedirectObject | undefined {
+  if(!urlpath || (urlpath[0] != '/')) urlpath = '/' + urlpath;
+
+  if(redirects && redirects.length){
+    for(var i=0;i<redirects.length;i++){
+      var redirect = redirects[i];
+      if(!redirect) continue;
+      var cmpurlpath = (redirect.redirect_url||'').toString();
+      var desturl = (redirect.redirect_dest||'').toString();
+      if(redirect.redirect_url_type=='EXACT'){
+        if(urlpath != cmpurlpath) continue;
+      }
+      else if(redirect.redirect_url_type=='EXACTICASE'){
+        if(urlpath.toLowerCase() != cmpurlpath.toLowerCase()) continue;
+      }
+      else if(redirect.redirect_url_type=='BEGINS'){
+        if(!urlpath.startsWith(cmpurlpath)) continue;
+      }
+      else if(redirect.redirect_url_type=='BEGINSICASE'){
+        if(!urlpath.toLowerCase().startsWith(cmpurlpath.toLowerCase())) continue;
+      }
+      else if((redirect.redirect_url_type=='REGEX')||(redirect.redirect_url_type=='REGEXICASE')){
+        var rxMatch = urlpath.match(new RegExp(cmpurlpath,((redirect.redirect_url_type=='REGEXICASE')?'i':'')));
+        if(!rxMatch) continue;
+        for(var j=rxMatch.length;j>=1;j--){
+          desturl = desturl.replaceAll('$'+j.toString(), rxMatch[j]);
+        }
+      }
+      return {
+        http_code: redirect.redirect_http_code,
+        url: desturl,
+      };
+    }
+  }
+  return undefined;
+}
+
 type RedirectEntry = {
   redirect_key: number,
   redirect_url: string,
   redirect_url_type: string, //'BEGINS'|'BEGINSICASE'|'EXACT'|'EXACTICASE'|'REGEX'|'REGEXICASE',
   redirect_dest: string,
   redirect_http_code: string, //'301'|'302'|'PASSTHRU',
+}
+
+type RedirectObject = {
+  http_code: string, //'301'|'302'|'PASSTHRU',
+  url: string,
 }
 
 export async function routePages(request: NextRequest) {
@@ -33,14 +84,12 @@ export async function routeRedirects(request: NextRequest) {
   }
 
   if (redirectData && redirectData.length > 0) {
-    const redirectEntry: RedirectEntry = Array.prototype.find.call(redirectData, function(entry: RedirectEntry) {
-      return entry.redirect_url == pathname;
-    });
-    if (redirectEntry) {
-      switch(redirectEntry.redirect_http_code) {
-        case '301': return NextResponse.redirect(new URL(redirectEntry.redirect_dest, request.url), 301);
-        case '302': return NextResponse.redirect(new URL(redirectEntry.redirect_dest, request.url), 302);
-        case 'PASSTHRU': return NextResponse.rewrite(new URL(redirectEntry.redirect_dest, request.url));
+    const redirectObject = matchRedirect(redirectData, pathname);
+    if (redirectObject) {
+      switch(redirectObject.http_code) {
+        case '301': return NextResponse.redirect(new URL(redirectObject.url, request.url), 301);
+        case '302': return NextResponse.redirect(new URL(redirectObject.url, request.url), 302);
+        case 'PASSTHRU': return NextResponse.rewrite(new URL(redirectObject.url, request.url));
       }
     }
   }
